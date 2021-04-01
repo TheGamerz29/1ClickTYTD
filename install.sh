3ff2b893b5259550e33f980f7a125135518b7032
StarFromTheSky <66721586+TheGamerz29@users.noreply.github.com>
2021-04-01 09:42:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,16 +4,17 @@
 # Licensed under GNU GPL 3.0
 
 echo -e "Checking and installing required package for installer..." 
-apt install -y ruby &> 1clickins.log && gem install lolcat
-apt install figlet -y &> 1clickins.log
+apt install -y ruby >> 1clickins.log && gem install lolcat >> 1clickins.log
+apt install figlet -y >> 1clickins.log
 clear
 
 figlet Installing
 figlet Dependancies | lolcat
 figlet ffmpeg | lolcat && echo "Installing..." 
-apt update &> 1clickins.log&& apt -y upgrade &> 1clickins.log
-yes | pkg install ffmpeg &> 1clickins.log
+apt update >> 1clickins3.log && apt -y upgrade >> 1clickins.log
+yes | pkg install ffmpeg >> 1clickins.log
 figlet Done | lolcat
+clear
 
 echo -e "Requesting access to storage\n"
 termux-setup-storage
@@ -21,12 +22,14 @@ sleep 5
 clear
 
 figlet Python | lolcat && echo "Installing... " 
-pkg install -y python &> 1clickins.log
+pkg install -y python >> 1clickins.log
 figlet Done | lolcat
+clear
 
 figlet Youtube-DL | lolcat && echo "Installing... " 
-yes | pip install youtube-dl &> 1clickins.log
+yes | pip install youtube-dl >> 1clickins.log
 figlet Done | lolcat
+clear
 
 echo -e "Creating youtube-dl folder for config\n"
 mkdir -p ~/.config/youtube-dl
@@ -36,4 +39,4 @@ mkdir ~/bin
 
 figlet 1ClickTYTD | lolcat && echo "Installing..." 
 mv termux-url-opener ~/bin/
-dos2unix ~/bin/termux-url-opener &> 1clickins.log
+dos2unix ~/bin/termux-url-opener >> 1clickins.log
